1745c937217b75b33e758f8b33981b86b281e908
goodutwo <goodutwo@gmail.com>
2017-12-21 20:42:39 +0800

    6

--- test.c
+++ test.c
@@ -7,5 +7,6 @@ int main(int argc, char* argv[])
     printf("hello world! goodutwo");
     printf("hello world! gooduthree from 10.95");
     printf("hello world! goodufour from 10.95");
+    printf("hello world! goodusix from 10.95");
     return 0;
 }
