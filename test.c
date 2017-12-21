419e1e696c85a468039875748e8eec60652f722b
goodutwo <goodutwo@gmail.com>
2017-12-21 19:29:54 +0800

    add 10.95

--- test.c
+++ test.c
@@ -5,5 +5,6 @@ int main(int argc, char* argv[])
     printf("hello world!");
     printf("hello world! gooduone");
     printf("hello world! goodutwo");
+    printf("hello world! gooduthree from 10.95");
     return 0;
 }
